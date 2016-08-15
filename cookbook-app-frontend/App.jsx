eaf97b320cb2ce14836100e94841f9d50694e610
admin <crema3624@gmail.com>
2016-08-14 19:56:46 -0500

    Adding styles and muyTheme

--- cookbook-app-frontend/App.jsx
+++ cookbook-app-frontend/App.jsx
@@ -1,35 +1,45 @@
 import React from 'react';
-import CategoryList from './model-list/CategoryList.jsx';
 
-import TextFieldFirstExample from './components/TextFieldFirstExample.jsx'
-
-//import injectTapEventPlugin from 'react-tap-event-plugin';
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-//injectTapEventPlugin();
+import CustomGridList from './components/GridList.jsx'
 
 class App extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            categories: []
+            categories: [
+                {
+                    "id": 1,
+                    "name": "Pastas",
+                    "img": '../images/grid-list/pastas.jpg'
+                }, {
+                    "id": 2,
+                    "name": "Salads",
+                    "img": '../images/grid-list/salads.jpg'
+                }, {
+                    "id": 3,
+                    "name": "Meat",
+                    "img": '../images/grid-list/meat.jpg'
+                }, {
+                    "id": 4,
+                    "name": "Desserts",
+                    "img": '../images/grid-list/desserts.jpg'
+                }
+            ]
         }
     }
-    componentWillMount() {
+    /*componentWillMount() {
         fetch('http://localhost:8000/api/categories').then((response) => {
             return response.json()
         }).then((categories) => {
             this.setState({categories: categories.data})
         })
-    }
+    }*/
     render() {
         if (this.state.categories.length > 0) {
             return (
                 <div>
-                    <TextFieldFirstExample/>
-                    <CategoryList listado={this.state.categories}/>
+                    <CustomGridList listado={this.state.categories}/>
                 </div>
             )
         } else {
--- cookbook-app-frontend/main.js
+++ cookbook-app-frontend/main.js
@@ -2,4 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-ReactDOM.render( < App / > , document.getElementById('app'));
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const CustomApp = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+);
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+ReactDOM.render( < CustomApp / > , document.getElementById('app'));
