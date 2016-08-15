7096d986699ad6856be9c69f9e8a07371cbe2581
Jorge E. Santacruz Portocarrero <jorge.santacruz@avantica.net>
2016-08-15 18:18:15 -0500

    App

--- cookbook-app-frontend/App.jsx
+++ cookbook-app-frontend/App.jsx
@@ -5,28 +5,28 @@ import CustomAppBar from './components/CustomAppBar.jsx';
 
 class App extends React.Component {
 
+    /*{
+      "id": 1,
+      "name": "Pastas",
+      "img": '../images/grid-list/pastas.jpg'
+  }, {
+      "id": 2,
+      "name": "Salads",
+      "img": '../images/grid-list/salads.jpg'
+  }, {
+      "id": 3,
+      "name": "Meat",
+      "img": '../images/grid-list/meat.jpg'
+  }, {
+      "id": 4,
+      "name": "Desserts",
+      "img": '../images/grid-list/desserts.jpg'
+  }*/
+
     constructor(props) {
         super(props)
         this.state = {
-            categories: [
-                {
-                    "id": 1,
-                    "name": "Pastas",
-                    "img": '../images/grid-list/pastas.jpg'
-                }, {
-                    "id": 2,
-                    "name": "Salads",
-                    "img": '../images/grid-list/salads.jpg'
-                }, {
-                    "id": 3,
-                    "name": "Meat",
-                    "img": '../images/grid-list/meat.jpg'
-                }, {
-                    "id": 4,
-                    "name": "Desserts",
-                    "img": '../images/grid-list/desserts.jpg'
-                }
-            ],
+            categories: [],
             appTitle: "Recipes",
             gridTitle: "Categories",
             open: false,
@@ -41,13 +41,24 @@ class App extends React.Component {
             ]
         }
     }
-    /*componentWillMount() {
-        fetch('http://localhost:8000/api/categories').then((response) => {
+
+    componentWillMount() {
+        const addPathImage = array => array.map(item => {
+            return {
+                ...item,
+                img: '../images/grid-list/' + item.name.toLowerCase() + '.jpg'
+            }
+        });
+
+        fetch('http://localhost:3000/cookbook/categories').then((response) => {
             return response.json()
         }).then((categories) => {
-            this.setState({categories: categories.data})
+            this.setState({
+                categories: addPathImage(categories.data)
+            })
         })
-    }*/
+    }
+
     handleClick = () => this.setState({
         open: !this.state.open
     });
